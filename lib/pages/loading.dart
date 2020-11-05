cd7bf0a931886f6357983b189206cecda4a0211e
Vasudev-2308 <56473514+Vasudev-2308@users.noreply.github.com>
2020-11-05 20:26:08 +0530

    Add files via upload

---
+++ lib/pages/loading.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:world_clock/services/worldtime.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Kolkata', flag: 'india.png', url: 'Asia/Kolkata');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitWave(
+              color: Colors.purple,
+              size: 80.0,
+            ),
+            Text(
+              "Loading...",
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
